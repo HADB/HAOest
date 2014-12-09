af23ab70b9dc3a5fcf0047716bd44f501e15c1b2
Bean <Bean.B.Deng@newegg.com>
2014-12-09 17:36:54 +0800

    add mobile.js

---
+++ src/js/mobile.js
@@ -0,0 +1,7 @@
+/* ========================================================================
+ * HAOest: mobile.js
+ * http://hadb.github.io/HAOest/javascript/#mobile
+ * ========================================================================
+ * Copyright 2014 HADB.
+ * Licensed under MIT (https://github.com/HADB/HAOest/blob/master/LICENSE)
+ * ======================================================================== */
